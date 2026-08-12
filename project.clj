80c792f8a083205499cb658db6af03df36f1c7de
Kyle Kingsbury <aphyr@aphyr.com>
2026-08-12 08:25:12 -0500

    Dependency exclusions

--- project.clj
+++ project.clj
@@ -3,9 +3,12 @@
   :url "http://yugabyte.com/"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-http "3.13.1" :exclusions [commons-logging]]
+  :dependencies [[clj-http "3.13.1" :exclusions [commons-logging
+                                                 commons-io
+                                                 potemkin]]
                  [jepsen "0.3.13"]
-                 [com.yugabyte/cassaforte "3.0.0-alpha2-yb-1"]
+                 [com.yugabyte/cassaforte "3.0.0-alpha2-yb-1"
+                  :exclusions [org.clojure/tools.reader]]
                  [org.clojure/java.jdbc "0.7.12"]
                  [org.postgresql/postgresql "42.7.13"]
                  [org.slf4j/jcl-over-slf4j "2.0.18"]
